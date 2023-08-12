0d3cd4c1fd2e093483b8c509950f1e2324683a93
Andrews0899 <andrewsmakafui350@gmail.com>
2023-08-11 19:03:29 -0700

    test files for base_model and file_storage

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,40 @@
+import unittest
+from unittest.mock import patch
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_id_is_string(self):
+        self.assertIsInstance(self.base_model.id, str)
+
+    def test_created_at_is_datetime(self):
+        self.assertIsInstance(self.base_model.created_at, datetime)
+
+    def test_updated_at_is_datetime(self):
+        self.assertIsInstance(self.base_model.updated_at, datetime)
+
+    def test_save_updates_updated_at(self):
+        old_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        new_updated_at = self.base_model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
+    def test_to_dict_contains_expected_keys(self):
+        expected_keys = ['id', 'created_at', 'updated_at', '__class__']
+        obj_dict = self.base_model.to_dict()
+        for key in expected_keys:
+            self.assertIn(key, obj_dict)
+
+    def test_str_representation(self):
+        expected_str = "[BaseModel] ({}) {}".format(self.base_model.id, self.base_model.__dict__)
+        self.assertEqual(str(self.base_model), expected_str)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
