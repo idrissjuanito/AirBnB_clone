48ccca7db0dfc31f9f251a04ab245935b9ac762e
idrissjuanito <idrissjuanito@gmail.com>
2023-08-13 22:31:45 +0000

    tried test solve checker fail

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" test module for city class """
+import unittest
+from models.city import City
+
+
+class TestCityClass(unittest.TestCase):
+    """ Test cases for attributes and methods in city class """
+    def test_city_name(self):
+        pass
